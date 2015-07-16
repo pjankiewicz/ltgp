f3af532c2b4938c9ef4acc9d482dc54bc0effe31
Pawel Jankiewicz <pawel.j@webinterpret.com>
2015-07-16 13:51:59 +0200

    chg

--- show.py
+++ show.py
@@ -13,27 +13,29 @@ EMPTY_CELL = "."
 ANT_CELL = "S"
 
 
-
 def pixel(surface, color, pos):
     surface.fill(color, (pos, (1, 1)))
 
-def draw_pixel(surface,pos,col,size=SCALE):
-    x,y = pos
+
+def draw_pixel(surface, pos, col, size=SCALE):
+    x, y = pos
     for x_ in range(size):
-        for y_ in range(size):        
-            surface.set_at((x-x_,y-y_),col)
+        for y_ in range(size):
+            surface.set_at((x-x_, y-y_), col)
+
 
 def draw_state(surface, ant):
     for x, row in enumerate(ant.matrix_exc):
         for y, col in enumerate(row):
             # FOOD
             if col == "food":
-                draw_pixel(surface, (x*SCALE,y*SCALE), CLR_FOOD)
+                draw_pixel(surface, (x*SCALE, y*SCALE), CLR_FOOD)
             elif col == "passed":
-                draw_pixel(surface, (x*SCALE,y*SCALE), CLR_PASSED)
-                
+                draw_pixel(surface, (x*SCALE, y*SCALE), CLR_PASSED)
+
     ant_pos = (ant.row*SCALE, ant.col*SCALE)
     draw_pixel(surface, ant_pos, CLR_ANT)
+
 
 def pprint(expr):
     indent = 0
@@ -48,21 +50,24 @@ def pprint(expr):
         if l == ",":
             sys.stdout.write("\n" + indent * '\t')
 
+
 def main():
-    ant, hof, routine = get_best_ant()   
-    pprint(str(hof).replace(" ",""))
-    
-    yes = raw_input("Continue?")    
-    
+    ant, hof, routine = get_best_ant()
+    pprint(str(hof).replace(" ", ""))
+
+    yes = raw_input("Continue?")
+
     ant._reset()
     scale = SCALE
     size = width, height = ant.matrix_row*SCALE, ant.matrix_col*SCALE
-    
+
     pygame.init()
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("ANTS")
     clock = pygame.time.Clock()
-    
+
+    iteration = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -71,11 +76,13 @@ def main():
         surface = pygame.Surface(size)
         surface.fill(CLR_BACKGROUND)
         draw_state(surface, ant)
-        screen.blit(surface, (0,0))
+        screen.blit(surface, (0, 0))
         pygame.display.update()
         clock.tick(FPS)
-        
+
         routine()
+
+        iteration += 1
 
 if __name__ == "__main__":
     main()
